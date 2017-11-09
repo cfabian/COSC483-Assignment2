302ff38966db65357081b9ceefa2dcd4a5b669fa
Nicklas <nredheff@vols.utk.edu>
2017-11-09 14:48:53 -0500

    prime works i think

--- functions.py
+++ functions.py
@@ -10,6 +10,40 @@ def powv1 (m, e, n):
       m = (m * m) % n
    return s
 
+
+def miller_rabin(n, k): #primetest
+
+    # Implementation uses the Miller-Rabin Primality Test
+    # The optimal number of rounds for this test is 40
+    # See http://stackoverflow.com/questions/6325576/how-many-iterations-of-rabin-miller-should-i-use-for-cryptographic-safe-primes
+    # for justification
+
+    # If number is even, it's a composite number
+
+    if n == 2:
+        return True
+
+    if n % 2 == 0:
+        return False
+
+    r, s = 0, n - 1
+    while s % 2 == 0:
+        r += 1
+        s //= 2
+    for _ in range(k):
+        a = random.randrange(2, n - 1)
+        x = pow(a, s, n)
+        if x == 1 or x == n - 1:
+            continue
+        for _ in range(r - 1):
+            x = pow(x, 2, n)
+            if x == n - 1:
+                break
+        else:
+            return False
+    return True
+
+
 def readFile(funName, fileName):
     try:
         with open(fileName, 'r') as f:
@@ -133,9 +167,14 @@ def modinv(a, m):
         return x % m
 
 def keygen(pubKeyFile, privKeyFile, numBits):
-    
-    p = number.getPrime(int(numBits))
-    q = number.getPrime(int(numBits))
+    numBits = int(numBits)
+    p = random.getrandbits(numBits)
+    q = random.getrandbits(numBits)
+    while not miller_rabin(p,100) and not miller_rabin(q,100):
+        p = random.getrandbits(numBits)
+        # p = number.getPrime(int(numBits))
+        # q = number.getPrime(int(numBits))
+        q = random.getrandbits(numBits)
     n = p * q
     order = (p - 1) * (q - 1)
     e = getCoprime(order)
